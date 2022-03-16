6b4128134ce24f85ae6e90782f3c1b9aa2e5ef98
SABIRASEHEER <sabirasabi465@gmail.com>
2022-03-16 15:19:14 +0530

    updated

--- 1-add2numbers.c
+++ 1-add2numbers.c
@@ -3,9 +3,9 @@
 void main()
 {
     int num1,num2;
-    printf("Enter first number \t");
+    printf("Enter first number is : \t");
     scanf("%d",&num1);
-    printf("Enter second number \t");
+    printf("Enter second number is :\t");
     scanf("%d",&num2);
     printf("Sum of %d and %d is %d",num1,num2,num1+num2);
 }
